1165e1e4181fa172dccca458aa82fb97216702e0
Luillin Escobar <contacto@contratistaweb.com>
2019-09-12 09:04:58 -0500

    agrupacion de la clase tecla

--- js/app.js
+++ js/app.js
@@ -1,20 +1,172 @@
+var teclas = document.getElementsByClassName('tecla')
+var pantalla = document.getElementById('display')
+var operando_a, operando_b, operacion;
+
 function Calculadora() {
-	var cero = document.getElementById('0')
-	var uno = document.getElementById('1')
-	var dos = document.getElementById('2')
-	var tres = document.getElementById('3')
-	var cuatro = document.getElementById('4')
-	var cinco = document.getElementById('5')
-	var seis = document.getElementById('6')
-	var siete = document.getElementById('7')
-	var ocho = document.getElementById('8')
-	var nueve = document.getElementById('9')
-	var sumar = document.getElementById('mas')
-	var restar = document.getElementById('menos')
-	var multiplicar = document.getElementById('por')
-	var dividir = document.getElementById('dividido')
-	var resultado = document.getElementById('igual')
-	var punto = document.getElementById('punto')
-	var onc = document.getElementById('on')
-	var signo = document.getElementById('sign')
+	//cero
+	teclas['15'].onclick = function(e){
+		if(pantalla.textContent=='0'){
+			pantalla.textContent = '0';
+		}else{
+			pantalla.textContent = pantalla.textContent + '0';
+		}
+		
+	}
+	//uno
+	teclas['12'].onclick = function(e){
+		if(pantalla.textContent=='0'){
+			pantalla.textContent = '1';
+		}else{
+			pantalla.textContent = pantalla.textContent + '1'
+		}
+		
+	}
+	//dos
+	teclas['13'].onclick = function(e){
+		if(pantalla.textContent=='0'){
+			pantalla.textContent = '2';
+		}else{
+			pantalla.textContent = pantalla.textContent + '2'
+		}
+	}
+	//tres 
+	teclas['14'].onclick = function(e){
+		if(pantalla.textContent=='0'){
+			pantalla.textContent = '3';
+		}else{
+			pantalla.textContent = pantalla.textContent + '3'
+		}
+	}
+	//cuatro
+	teclas['8'].onclick = function(e){
+		if(pantalla.textContent=='0'){
+			pantalla.textContent = '4';
+		}else{
+			pantalla.textContent = pantalla.textContent + '4'
+		}
+	}
+	//cinco
+	teclas['9'].onclick = function(e){
+		if(pantalla.textContent=='0'){
+			pantalla.textContent = '5';
+		}else{
+			pantalla.textContent = pantalla.textContent + '5'
+		}
+	}
+	//seis
+	teclas['10'].onclick = function(e){
+		if(pantalla.textContent=='0'){
+			pantalla.textContent = '6';
+		}else{
+			pantalla.textContent = pantalla.textContent + '6'
+		}
+	}
+	//siete
+	teclas['4'].onclick = function(e){
+		if(pantalla.textContent=='0'){
+			pantalla.textContent = '7';
+		}else{
+			pantalla.textContent = pantalla.textContent + '7'
+		}
+	}
+	//ocho
+	teclas['5'].onclick = function(e){
+		if(pantalla.textContent=='0'){
+			pantalla.textContent = '8';
+		}else{
+			pantalla.textContent = pantalla.textContent + '8'
+		}
+	}
+	//nueve
+	teclas['6'].onclick = function(e){
+		if(pantalla.textContent=='0'){
+			pantalla.textContent = '9';
+		}else{
+			pantalla.textContent = pantalla.textContent + '9'
+		}
+	}
+	// punto
+	teclas['16'].onclick = function(e){
+		if(pantalla.textContent == '0'){
+			pantalla.textContent = pantalla.textContent + '.'
+		}
+		if(pantalla.textContent == '.'){
+			pantalla.textContent = pantalla.textContent
+		}
+	}
+	// menos
+	teclas['1'].onclick = function(e){
+		if(pantalla.textContent=='0'){
+			pantalla.textContent = '-' + pantalla.textContent
+		}else{
+			pantalla.textContent = pantalla.textContent*(-1)
+		}
+	}
+	// On/C
+	teclas['0'].onclick = function(e){
+		resetear();
+	}
+	//suma
+	teclas['18'].onclick = function(e){
+		operando_a = pantalla.textContent;
+		operacion = '+';
+		limpiar();
+	}
+	//resta
+	teclas['11'].onclick = function(e){
+		operando_a = pantalla.textContent;
+		operacion = '-';
+		limpiar();
+	}
+	//multiplicacion
+	teclas['7'].onclick = function(e){
+		operando_a = pantalla.textContent;
+		operacion = '*';
+		limpiar();
+	}
+	//division
+	teclas['3'].onclick = function(e){
+		operando_a = pantalla.textContent;
+		operacion = '/';
+		limpiar();
+	}
+	//igual
+	teclas['17'].onclick = function(e){
+			operando_b = pantalla.textContent;
+			resultado();
+	}
 }
+
+function limpiar(){
+	pantalla.textContent = '';
+}
+
+function resetear(){
+	pantalla.textContent = '0';
+	operando_a = '';
+	operando_b = '';
+	operacion = ''
+}
+
+function resultado(){
+	var res = 0
+	
+	switch(operacion){
+		case '+':
+			res = parseFloat(operando_a) + parseFloat(operando_b);
+			break;
+			case '-':
+			res = parseFloat(operando_a) - parseFloat(operando_b);
+			break;
+			case '*':
+			res = parseFloat(operando_a) * parseFloat(operando_b);
+			break;
+			case '/':
+			res = parseFloat(operando_a) / parseFloat(operando_b);
+			break;
+									}
+	resetear();
+	pantalla.textContent = res.toPrecision(8);
+}
+
+	Calculadora();
