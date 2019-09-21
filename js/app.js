e3fb8433c1cf4ac75c59d92dbf7efc8291b576a9
Luillin Escobar <contacto@contratistaweb.com>
2019-09-21 02:56:29 -0500

    pendiente el efecto presionar teclas

--- js/app.js
+++ js/app.js
@@ -1,83 +1,101 @@
-var pantalla = document.getElementById('display');
-var num_a, num_b, operacion;
-var teclas = document.querySelectorAll('.teclado .tecla');
 
 Calculadora();
 function Calculadora() {
-for(var i=0;i<=teclas.length-1;i++){
-	teclas[i].addEventListener("click", function(tecla){
-		var x = tecla.target;
-		if(x.id <= 9){
-			if(pantalla.textContent == 0){
-				pantalla.textContent = x.id;
+	var pantalla = document.getElementById('display');
+	var num_a, num_b, operacion;
+	var teclas = document.querySelectorAll('.teclado .tecla');
+	for(var i=0;i<=teclas.length-1;i++){
+		teclas[i].addEventListener("click", function(tecla){
+			var x = tecla.target;
+			if(x.id <= 9){
+				if(pantalla.textContent == '0'){
+					pantalla.textContent = x.id;
+				}else{
+					if(pantalla.textContent.length <= 7 ||  pantalla.textContent == '0.'){
+						pantalla.textContent = pantalla.textContent + x.id;
+					}
+				}
 			}else{
-				if(pantalla.textContent.length <= 7){
-					pantalla.textContent = pantalla.textContent + x.id;
+				if(x.id == 'mas' || x.id == 'menos' || x.id == 'por'  || x.id == 'dividido'){
+					operacion = x.id;
+					num_a = pantalla.textContent;
+					limpiar();
+					function limpiar(){
+						pantalla.textContent = '';
+					}
+				}else{
+					if(x.id == 'on'){
+						resetear();
+					}else{
+						if(x.id == 'sign'){
+							pantalla.textContent = pantalla.textContent*(-1);
+						}else{
+							if(x.id == 'igual'){
+								num_b = pantalla.textContent;
+								igual();
+							}else{
+								if(x.id == 'punto'){
+									if(pantalla.textContent == ""){
+										pantalla.textContent = pantalla.textContent + '0.';
+									}else{
+										if(pantalla.textContent.split(".").length-1 == 0) {
+											pantalla.textContent = pantalla.textContent + '.';
+										}else{
+											pantalla.textContent = pantalla.textContent ;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+				function resetear(){
+					pantalla.textContent = '0';
+					num_a = '0';
+					num_b = '0';
+					operacion = ''
+				}
+				function igual(){
+					var respuesta = '';
+					switch(operacion){
+						case 'mas':
+							respuesta = parseFloat(num_a) + parseFloat(num_b);
+							break;
+						case 'menos':
+							respuesta = parseFloat(num_a) - parseFloat(num_b);
+							break;
+						case 'por':
+							respuesta = parseFloat(num_a) * parseFloat(num_b);
+							break;
+						case 'dividido':
+							respuesta = parseFloat(num_a) / parseFloat(num_b);
+							break;
+													}
+					pantalla.textContent =respuesta
+					if(pantalla.textContent.length <= 7){
+						pantalla.textContent;
+					}else{
+						var ent = respuesta.toFixed(0);
+						pantalla.textContent = respuesta.toFixed(7 - ent.length);
+						// resultado con decimales
+					}
 				}
 			}
-		}else{
-		if(x.id == 'mas' || x.id == 'menos' || x.id == 'por'  || x.id == 'dividido'){
-			operacion = x.id;
-			num_a = pantalla.textContent;
-			limpiar();
-			function limpiar(){
-				pantalla.textContent = '';
-			}
-		}else{
-			if(x.id == 'on'){
-				resetear();
-			}else{
-				if(x.id == 'sign'){
-					 pantalla.textContent = pantalla.textContent*(-1);
-					 }else{
-						 if(x.id == 'igual'){
-							num_b = pantalla.textContent;
-							 igual();
-								}else{
-									if(x.id == 'punto'){
-										 		if(pantalla.textContent == ""){
-													pantalla.textContent = pantalla.textContent + '0.';
-												}else{
-													if( pantalla.textContent.split(".").length-1 == 0) {
-												pantalla.textContent = pantalla.textContent + '.';
-													}else{
-														pantalla.textContent = pantalla.textContent ;
-													}
-												}
-										 }
-								}
-					 }
-			}
-		}
-			function resetear(){
-				pantalla.textContent = '0';
-				num_a = '0';
-				num_b = '0';
-				operacion = ''
-			}
-		
-			function igual(){
-			var respuesta = '';
-				switch(operacion){
-					case 'mas':
-						respuesta = num_a - num_b;
-						break;
-					case 'menos':
-						respuesta = num_a - num_b;
-						break;
-					case 'por':
-						respuesta = num_a * num_b;
-						break;
-					case 'dividido':
-						respuesta = num_a / num_b;
-						break;
-									}
-			pantalla.textContent = respuesta;
-			}
-		
-		}
-	});
-};
-
+				
+												//<---   --->
+		function mouseDown() {
+  x.style ='border-radius:25px';
 }
 
+function mouseUp() {
+  x.style ='border-radius:0px';
+}
+		mouseDown();
+					mouseUp();
+				//<----   --->
+
+				
+				
+		});
+	}
+}
